ae4adb30a74bb6bc5e5b7c862e5560b6bc01baa5
Johnny Villegas <linkrsfan@gmail.com>
2019-06-06 01:37:18 -0500

    Add files via upload

---
+++ COJ_4302.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define fp first
+#define sp second
+#define mp make_pair
+#define ins insert
+#define uEdge(u, v) g[u].pb(v), g[v].pb(u)
+#define uwEdge(u, v, w) g[u].pb({v, w}), g[v].pb({u, w})
+#define dEdge(u, v) g[u].pb(v)
+#define dwEdge(u, v, w) g[u].pb({v, w})
+#define BOOST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define minHeap int, vector<int>, greater<int>
+
+using namespace std;
+typedef long long int lli;
+typedef pair<int, int> pii;
+vector<int> g[0];
+
+lli gcd(lli a, lli b){
+    if(b == 0) return a;
+    a%=b;
+    return gcd(b, a);
+}
+
+lli lcm(lli a, lli b){
+    return (a*b)/gcd(a, b);
+}
+
+int main(){
+    int tc, x, y;
+    scanf("%d", &tc);
+    for(int i = 0; i < tc; i++){
+        scanf("%d %d", &x, &y);
+        printf("%d\n", lcm(x, y));
+    }
+    return 0;
+}
